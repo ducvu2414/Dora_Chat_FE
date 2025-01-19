b37f17eab8c847aff722617dd7f3d8e1675b6e77
PhamVanThanh <phamtrung21112003@gmail.com>
2025-01-19 13:13:57 +0700

    add Dropdown

--- src/components/ui/search-bar.tsx
+++ src/components/ui/search-bar.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Search, Plus } from 'lucide-react'
 import { Input } from "./input"
 import { Button } from "./button"
+import { Dropdown } from "./dropdown"
 
 export function SearchBar() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false)
+
   return (
     <div className="relative flex items-center gap-2 p-2">
       <div className="relative flex-1">
@@ -11,12 +14,23 @@ export function SearchBar() {
         <Input 
           type="search" 
           placeholder="Search" 
-          className="pl-9 bg-gray-50 text-regal-blue placeholder:text-regal-blue rounded-full "
+          className="pl-9 bg-gray-50 text-regal-blue placeholder:text-regal-blue rounded-full border-regal-blue"
         />
       </div>
-      <Button size="icon" className="shrink-0 rounded-full bg-gradient-to-r from-sky-700 from-5% via-blue-400 via-55% to-blue-300 to-90% text-white">
-        <Plus className="!h-6 !w-6" />
-      </Button>
+      <div className="relative">
+        <Button 
+          size="icon" 
+          className="shrink-0 rounded-full bg-gradient-to-r from-sky-700 from-5% via-blue-400 via-55% to-blue-300 to-90% text-white hover:scale-105"
+          onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+        >
+          <Plus className="!h-6 !w-6" />
+        </Button>
+        <Dropdown 
+          isOpen={isDropdownOpen} 
+          onClose={() => setIsDropdownOpen(false)} 
+        />
+      </div>
     </div>
   )
 }
+
