0fee19a3da03e382fa260b965b8e78face0bd389
GGDuck <vunguyenminhduc1410@gmail.com>
2025-01-20 22:37:09 +0700

    Update Route.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import "./App.css"
-import LoginPage from "./page/LoginPage"
-import SignUpPage from "./page/SignUpStep1Page"
-import HomePage from "./page/HomePage"
-import SignUpStep2Page from "./page/SignUpStep2Page"
-import { ToastContainer } from "react-toastify"
-import SignUpStep3Page from "./page/SignUpStep3Page"
-import SignUpStep4Page from "./page/SignUpStep4Page"
-import UserInformationPage from "./page/UserInformationPage"
-import ContactsPage from "./page/ContactsPage"
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { ToastContainer } from "react-toastify";
+import routes from "./routes/route";
 
 function App() {
   return (
@@ -25,20 +19,15 @@ function App() {
         pauseOnHover
         theme="light"
       />
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/signup/otp" element={<SignUpStep2Page />} />
-        <Route path="/signup/info" element={<SignUpStep3Page />} />
-        <Route path="/signup/complete" element={<SignUpStep4Page />} />
-        <Route path="/forgot-password" element={<div>Forgot Password Page</div>} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/user-information" element={<UserInformationPage />} />
-        <Route path="/contacts" element={<ContactsPage />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {routes.map(({ path, element }, index) => (
+            <Route key={index} path={path} element={element} />
+          ))}
+        </Routes>
+      </Suspense>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/routes/route.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const LoginPage = React.lazy(() => import("../page/LoginPage"));
+const SignUpPage = React.lazy(() => import("../page/SignUpStep1Page"));
+const HomePage = React.lazy(() => import("../page/HomePage"));
+const SignUpStep2Page = React.lazy(() => import("../page/SignUpStep2Page"));
+const SignUpStep3Page = React.lazy(() => import("../page/SignUpStep3Page"));
+const SignUpStep4Page = React.lazy(() => import("../page/SignUpStep4Page"));
+const UserInformationPage = React.lazy(() =>
+    import("../page/UserInformationPage")
+);
+const ContactsPage = React.lazy(() => import("../page/ContactsPage"));
+
+const routes = [
+    { path: "/", element: <LoginPage /> },
+    { path: "/login", element: <LoginPage /> },
+    { path: "/signup", element: <SignUpPage /> },
+    { path: "/signup/otp", element: <SignUpStep2Page /> },
+    { path: "/signup/info", element: <SignUpStep3Page /> },
+    { path: "/signup/complete", element: <SignUpStep4Page /> },
+    { path: "/forgot-password", element: <div>Forgot Password Page</div> },
+    { path: "/home", element: <HomePage /> },
+    { path: "/user-information", element: <UserInformationPage /> },
+    { path: "/contacts", element: <ContactsPage /> },
+];
+
+export default routes;
