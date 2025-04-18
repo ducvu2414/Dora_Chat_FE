be33aad9e640b1c339fdb0dfb663c51e412a0586
Phạm Văn Thành <21089891.thanh@student.iuh.edu.vn>
2025-04-18 15:16:08 +0700

    check friend

--- src/api/friend.js
+++ src/api/friend.js
@@ -1,50 +1,58 @@
 import axios from "./axiosClient";
 
 const friendApi = {
-    fetchFriends: (id, name) => {
-        console.log(id, name);
-        return axios.get(`/api/friends`, {
-            params: {
-                _id: id,
-                name,
-            },
-        });
-    },
-    acceptRequestFriend: (userId) => {
-        return axios.post(`/api/friends/${userId}`);
-    },
+  fetchFriends: (id, name) => {
+    console.log(id, name);
+    return axios.get(`/api/friends`, {
+      params: {
+        _id: id,
+        name,
+      },
+    });
+  },
+  acceptRequestFriend: (userId) => {
+    return axios.post(`/api/friends/${userId}`);
+  },
 
-    deleteFriend: (userId) => {
-        return axios.delete(`/api/friends/${userId}`);
-    },
+  deleteFriend: (userId) => {
+    return axios.delete(`/api/friends/${userId}`);
+  },
 
-    fetchListRequestFriend: () => {
-        return axios.get(`/api/friends/invites`);
-    },
+  fetchListRequestFriend: () => {
+    return axios.get(`/api/friends/invites`);
+  },
 
-    deleteRequestFriend: (userId) => {
-        return axios.delete(`/api/friends/invites/${userId}`);
-    },
+  deleteRequestFriend: (userId) => {
+    return axios.delete(`/api/friends/invites/${userId}`);
+  },
 
-    sendRequestFriend: (userId) => {
-        return axios.post(`/api/friends/invites/me/${userId}`);
-    },
+  sendRequestFriend: (userId) => {
+    return axios.post(`/api/friends/invites/me/${userId}`);
+  },
 
-    deleteSentRequestFriend: (userId) => {
-        return axios.delete(`/api/friends/invites/me/${userId}`);
-    },
+  deleteSentRequestFriend: (userId) => {
+    return axios.delete(`/api/friends/invites/me/${userId}`);
+  },
 
-    fetchMyRequestFriend: () => {
-        return axios.get(`/api/friends/invites/me`);
-    },
-    fetchSuggestFriend: (page = 0, size = 12) => {
-        return axios.get(`/api/friends/suggest`, {
-            params: {
-                page,
-                size,
-            },
-        });
-    },
+  fetchMyRequestFriend: () => {
+    return axios.get(`/api/friends/invites/me`);
+  },
+  fetchSuggestFriend: (page = 0, size = 12) => {
+    return axios.get(`/api/friends/suggest`, {
+      params: {
+        page,
+        size,
+      },
+    });
+  },
+  isFriend: (userId1, userId2) => {
+    return axios.get(`/api/friends/is-friend`, {
+      params: {
+        userId1,
+        userId2,
+      },
+    });
+  },
 };
 
 export default friendApi;
